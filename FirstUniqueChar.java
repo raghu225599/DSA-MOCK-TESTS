48d216df1fc2975d22e34d98cc4b33db278c02a1
MAGANTI SRI VENKATA SAI RAGHUVEER <70837141+raghu225599@users.noreply.github.com>
2023-06-06 00:02:01 +0530

    FirstUniqueChar.java

--- FirstUniqueChar.java
+++ FirstUniqueChar.java
@@ -1,3 +1,21 @@
+/** Given a string s, find the first non-repeating character in it and return its index. If it does not exist, return -1.
+
+Example 1:
+Input: s = "leetcode"
+Output: 0
+
+Example 2:
+Input: s = "loveleetcode"
+Output: 2
+
+Example 3:
+Input: s = "aabb"
+Output: -1
+
+Constraints:
+a. 1 <= s.length <= 10^5
+b. s consists of only lowercase English letters.
+*/
 public class FirstUniqueChar {
     public static void main(String[] args) {
         String s = "leetcode";
